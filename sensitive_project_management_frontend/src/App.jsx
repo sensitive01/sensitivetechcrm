2490d1ceed675b0ea88c36977858edbd92cf42d4
Pujasmn <vaktisa2000@gmail.com>
2024-12-06 14:14:23 +0530

    test again

--- sensitive_project_management_frontend/src/App.jsx
+++ sensitive_project_management_frontend/src/App.jsx
@@ -5,20 +5,18 @@ function App() {
   return (
     <>
      <h1>Project Management</h1>
-<<<<<<< HEAD
       <h1>Project Management- rakesh code</h1>
       <h1>Project Management- rakesh code</h1>
       <h1>Project Management- rakesh code</h1>
 
       <h1>Project Management- rakesh code</h1>
       
-=======
      <h1>Project Management-puja</h1>
      <h1>Project Management-puja</h1>
      <h1>Project Management-puja</h1>
      <h1>Project Management-puja</h1>
+     <h2>Project Management-pujahjjhhj</h2>
 
->>>>>>> Puja_code
       
     </>
   )
