0990051849cb870b66c90f4a758a165a8466375f
rakeshn16 <rakeshnedagotti@gmail.com>
2024-12-07 11:28:20 +0530

    login and signup done

--- sensitive_project_management_frontend/src/App.jsx
+++ sensitive_project_management_frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Topbar from "./components/Topbar/Topbar";
 import Test from "./components/Test";
+import LoginPage from "./components/Login/Login";
+import SignUp from "./assets/SignUp";
 
 function App() {
   return (
@@ -12,6 +14,10 @@ function App() {
         {/* Define your routes here */}
         <Route path="/" element={<Dashboard />} />
         <Route path="/user" element={<Test />} />
+        <Route path="/login" element={<LoginPage/>} />
+
+        <Route path="/signup" element={<SignUp />} />
+
         {/* Add additional routes for other components */}
       </Routes>
     </Router>
