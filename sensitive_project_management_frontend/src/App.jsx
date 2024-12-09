93aa69488f0511cf147cb1903739c4d0cae0ac30
Pujasmn <vaktisa2000@gmail.com>
2024-12-09 10:01:50 +0530

    modify routing

--- sensitive_project_management_frontend/src/App.jsx
+++ sensitive_project_management_frontend/src/App.jsx
@@ -1,43 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Topbar from "./components/Topbar/Topbar";
 import EmployeeTable from "./components/Employee/EmployeeTable";
 import Employee from "./components/Employee/Employee";
 import LoginPage from "./components/Login/Login";
 import SignUp from "./assets/SignUp";
-import TaskList from "./components/Task/Task"; // Make sure to import your TaskList component
+import TaskList from "./components/Task/Task";
+
+// Layout Component for Admin Pages
+const AdminLayout = ({ children }) => (
+  <div className="app">
+    <Topbar />
+    <div className="main-content">{children}</div>
+  </div>
+);
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Define your routes here */}
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/employee-table" element={<EmployeeTable />} />
-        <Route path="/employee-form" element={<Employee />} />
-        <Route path="/login" element={<LoginPage/>} />  
+        {/* Public Routes */}
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/task" element={<TaskList />} /> {/* Route for Task Management Page */}
-        {/* Add additional routes for other components */}
-        {/* Login Route - No Topbar */}
-        <Route path="/" element={<LoginPage />} />
 
-        {/* Admin Layout - With Topbar */}
+        {/* Admin Routes */}
         <Route
           path="/*"
           element={
-            <div className="app">
-              <Topbar />
-              <div className="main-content">
-                <Routes>
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/employee-table" element={<EmployeeTable />} />
-                  <Route path="/employee-form" element={<Employee />} />
-                </Routes>
-              </div>
-            </div>
+            <AdminLayout>
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/employee-table" element={<EmployeeTable />} />
+                <Route path="/employee-form" element={<Employee />} />
+                <Route path="/task" element={<TaskList />} />
+              </Routes>
+            </AdminLayout>
           }
         />
       </Routes>
